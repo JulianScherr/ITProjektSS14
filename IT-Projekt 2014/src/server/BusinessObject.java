db910ae532abea9892a014298a459518064c5aba
Stefan <so025@hdm-stuttgart.de>
2014-04-24 16:23:14 +0200

    Anlegen BusinessObject Klasse Anlegen Nutzer Klasse Anlegen Package server

---
+++ IT-Projekt 2014/src/server/BusinessObject.java
@@ -0,0 +1,19 @@
+package server;
+
+import java.io.Serializable;
+
+
+
+
+
+public abstract class BusinessObject implements Serializable {
+
+private Date erstellungszeitpunkt = 0;
+
+
+
+public Date getErstellungszeitpunkt(){}
+public String toString(){}
+public boolean equals(){}
+public int hascode(){}
+}
---
+++ IT-Projekt 2014/src/server/Nutzer.java
@@ -0,0 +1,20 @@
+package server;
+
+public class Nutzer extends BusinessObject {
+private String vorname = "";
+private String nachname ="";
+private String nickname = "";
+
+public String getVorname() {
+	return vorname;
+}
+public String getNachname() {
+	return nachname;
+}
+public String getnickname(){
+	return nickname;
+}
+
+
+
+}
