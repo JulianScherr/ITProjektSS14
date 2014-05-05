7c31168cc72590335ae8846a6d1d895e9d706403
MarianSchrempf <ms350@hdm-stuttgart.de>
2014-05-05 12:28:17 +0200

    server.db package angelegt

---
+++ IT-Projekt 2014/src/de/hdm/itprojekt/server/db/DBConnectionLocal.java
@@ -0,0 +1,42 @@
+package de.hdm.itprojekt.server.db;
+
+import java.sql.*;
+
+public class DBConnectionLocal { 
+    
+    public static void main(String[] args) { 
+ 
+        // Diese Eintraege werden zum 
+        // Verbindungsaufbau benoetigt. 
+        final String hostname = "localhost"; 
+        final String port = "3306"; 
+        final String dbname = "socialmediapinnwand"; 
+        final String user = "root@localhost"; 
+        final String password = "";
+    }
+        private static Connection con = null
+	
+        try { 
+	    System.out.println("* Treiber laden"); 
+      	    Class.forName(("com.mysql.jdbc.Driver").newInstance(); 
+        } 
+        catch (Exception e) { 
+            System.err.println("Treiber kann nicht geladen werden!"); 
+            e.printStackTrace(); 
+        } 
+        try { 
+	    System.out.println("* Verbindung aufbauen"); 
+	    con = DriverManager.getConnection("jdbc:mysql://localhost/socialmediapinnwand","",""); 
+	    
+	    System.out.println("* Datenbank-Verbindung beenden"); 
+	    conn.close(); 
+        } 
+        catch (Exception e) { 
+            System.out.println("Exception Fehler: " + e.getMessage()); 
+         
+        } 
+        return con;
+		
+     } 
+    
+
